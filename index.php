656a31db228f521b0c54c215f8e328bab30af4b0
Julien <lissjulieng@mail.com>
2023-03-16 11:19:32 +0100

    Add and update files for main page

--- index.php
+++ index.php
@@ -1,49 +1,62 @@
 <?php include("header.php"); ?>
 
-<p id="subtitle">
-    Iron Man est un film américano-canadien réalisé par Jon Favreau, sorti en 2008.<br><br>
-    Il raconte les origines et les débuts du personnage éponyme issu du comics publié.
-</p>
-<div class="slider">
-    <div class="slides">
-        <!--radio buttons start-->
-        <input type="radio" name="radio-btn" id="radio1">
-        <input type="radio" name="radio-btn" id="radio2">
-        <input type="radio" name="radio-btn" id="radio3">
-        <input type="radio" name="radio-btn" id="radio4">
-        <!--radio buttons end-->
-        <!--slide images start-->
-        <div class="slide first">
-            <img src="1.jpg" alt="">
+<main>
+    <h1>IRON MAN</h1>
+    <p id="subtitle">
+        Iron Man est un film américano-canadien réalisé par Jon Favreau, sorti en 2008.<br><br>
+        Il raconte les origines et les débuts du personnage éponyme issu du comics publié.
+    </p>
+    <section class="center">
+        <div class="slider">
+            <div class="slides">
+                <!--radio buttons start-->
+                <input type="radio" name="radio-btn" id="radio1">
+                <input type="radio" name="radio-btn" id="radio2">
+                <input type="radio" name="radio-btn" id="radio3">
+                <input type="radio" name="radio-btn" id="radio4">
+                <!--radio buttons end-->
+                <!--slide images start-->
+                <div class="slide first">
+                    <img src="assets/images/1.jpg" alt="">
+                </div>
+                <div class="slide">
+                    <img src="assets/images/2.jpg" alt="">
+                </div>
+                <div class="slide">
+                    <img src="assets/images/3.jpg" alt="">
+                </div>
+                <div class="slide">
+                    <img src="assets/images/4.jpg" alt="">
+                </div>
+            </div>
+            <!--manual navigation start-->
+            <div class="navigation-manual">
+                <label for="radio1" class="manual-btn"></label>
+                <label for="radio2" class="manual-btn"></label>
+                <label for="radio3" class="manual-btn"></label>
+                <label for="radio4" class="manual-btn"></label>
+            </div>
+            <!--manual navigation end-->
         </div>
-        <div class="slide">
-            <img src="2.jpg" alt="">
+    </section>
+    <section class="main-container center">
+        <div class="main-wrapper">
+            <a href="#" class="link-box">
+                <div class="main-box"><h2 class="main-title">Mon portfolio</h2></div>
+            </a>
+            <a href="#" class="link-box">
+                <div class="main-box"><h2 class="main-title">Mes expériences</h2></div>
+            </a>
+            <a href="#" class="link-box">
+                <div class="main-box"><h2 class="main-title">Mes formations</h2></div>
+            </a>
+            <a href="#" class="link-box">
+                <div class="main-box"><h2 class="main-title">Contactez-moi</h2></div>
+            </a>
         </div>
-        <div class="slide">
-            <img src="3.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="4.jpg" alt="">
-        </div>
-        <!--slide images end-->
-        <!--automatic navigation start-->
-        <div class="navigation-auto">
-            <div class="auto-btn1"></div>
-            <div class="auto-btn2"></div>
-            <div class="auto-btn3"></div>
-            <div class="auto-btn4"></div>
-        </div>
-        <!--automatic navigation end-->
-    </div>
-    <!--manual navigation start-->
-    <div class="navigation-manual">
-        <label for="radio1" class="manual-btn"></label>
-        <label for="radio2" class="manual-btn"></label>
-        <label for="radio3" class="manual-btn"></label>
-        <label for="radio4" class="manual-btn"></label>
-    </div>
-    <!--manual navigation end-->
-</div>
+    </section>
+</main>
+
 <!-- slider JS -->
 <script type="text/javascript">
     var counter = 1;
